338a2abc54bef22d161e97bd85c3afe195a96e22
Yate9 <lucasmourisabeijon06@liceolapaz.net>
2026-06-04 09:30:43 +0200

    acabando

--- src/edu/lucas/examenfinal/GestorClub.java
+++ src/edu/lucas/examenfinal/GestorClub.java
@@ -3,8 +3,8 @@ package edu.lucas.examenfinal;
 import java.util.ArrayList;
 
 public class GestorClub {
-    static final double PRESUPUESTO_MAXIMO=100000;
-    static ArrayList<Profesional> profesionales=new ArrayList<>();
+    static final double PRESUPUESTO_MAXIMO = 10000;
+    static ArrayList<Profesional> profesionales = new ArrayList<>();
 
     public static ArrayList<Profesional> getProfesionales() {
         return profesionales;
@@ -14,15 +14,53 @@ public class GestorClub {
         GestorClub.profesionales = profesionales;
     }
 
-    public static void contratarJugador(Jugador jugador)throws PresupuestoExcedidoException{
-        double salarioTotal=0;
-        for(Profesional profesional:profesionales){
-            salarioTotal+=profesional.getSalarioBase();
+    public static void contratarJugador(Jugador jugador) throws PresupuestoExcedidoException {
+        double salarioTotal = 0;
+        for (Profesional profesional : profesionales) {
+            salarioTotal += profesional.getSalarioBase();
         }
-        if(salarioTotal+jugador.getSalarioBase()>PRESUPUESTO_MAXIMO){
+        if (salarioTotal + jugador.getSalarioBase() > PRESUPUESTO_MAXIMO) {
             throw new PresupuestoExcedidoException("Presupuesto excedido");
         }
-        salarioTotal+=jugador.getSalarioBase();
         profesionales.add(jugador);
+    }
+
+    public static void contratarTecnico(Tecnico tecnico) throws PresupuestoExcedidoException {
+        double salarioTotal = 0;
+        for (Profesional profesional : profesionales) {
+            salarioTotal += profesional.getSalarioBase();
+        }
+        if (salarioTotal + tecnico.getSalarioBase() > PRESUPUESTO_MAXIMO) {
+            throw new PresupuestoExcedidoException("Presupuesto excedido");
+        }
+        profesionales.add(tecnico);
+    }
+
+    public static void despedirProfesional(String nombreProfesional) throws ProfesionalNoEncontradoException {
+        for (Profesional profesional : profesionales) {
+            if (profesional.getNombre().equalsIgnoreCase(nombreProfesional)) {
+                profesionales.remove(profesional);
+            } else {
+                throw new ProfesionalNoEncontradoException("Profesional no encontrado");
+            }
+        }
+    }
+
+    public static void mostrarNominas() {
+        for (Profesional profesional : profesionales) {
+            double salarioTotal =0;
+            if (profesional instanceof Jugador) {
+                System.out.println("Jugador: " + profesional.getNombre() + " con la posicion " + ((Jugador) profesional).getPosicion() + " y un salario de " + profesional.getSalarioBase() + profesional.getPlusSalarial());
+                salarioTotal += (profesional.getSalarioBase()+(profesional.getPlusSalarial()));
+            } else {
+                System.out.println("Tecnico: " + profesional.getNombre() + " con el puesto " + ((Tecnico) profesional).getPuesto() + " y un salario de " + profesional.getSalarioBase() + profesional.getPlusSalarial());
+                salarioTotal += (profesional.getSalarioBase()+(profesional.getPlusSalarial()));
+            }
+        }
+    }
+    public static void mostrarProfesionales(){
+        for(Profesional profesional:profesionales){
+            System.out.println(profesional.getNombre());
+        }
     }
 }
--- src/edu/lucas/examenfinal/Main.java
+++ src/edu/lucas/examenfinal/Main.java
@@ -16,33 +16,62 @@ public class Main {
             System.out.println("3. Despedir profesional");
             System.out.println("4. Ver nominas totales");
             System.out.println("5. Salir");
+            System.out.println("6. Ver profesionales");
             int opcion=sc.nextInt();
             switch(opcion){
                 case 1:
                     System.out.println("Contratatando jugador");
                     System.out.println("Ingrese el nombre del jugador");
-                    String nombre=sc.next();
+                    String nombreJugador=sc.next();
                     System.out.println("Ingrese el salario base del jugador");
-                    double salario=sc.nextDouble();
+                    double salarioBaseJugador=sc.nextDouble();
                     System.out.println("Ingrese la posicion del jugador(PORTERO,DEFENSA,CENTROCAMPISTA,DELANTERO)");
-                    Posicion posicion=Posicion.valueOf(sc.next());
-                    Jugador jugador=new Jugador(nombre,salario,posicion);
-                    GestorClub.contratarJugador(jugador);
+                    Posicion posicionJugador=Posicion.valueOf(sc.next());
+                    Jugador jugador=new Jugador(nombreJugador,salarioBaseJugador,posicionJugador);
+                    try{
+                        GestorClub.contratarJugador(jugador);
+                    }catch(PresupuestoExcedidoException e){
+                        System.err.println("Presupuesto excedido");
+                        System.err.println("Fichaje del jugador cancelado");
+                    }
                     break;
 
                 case 2:
-                    System.out.println("Contratar Cuerpo Tecnico");
+                    System.out.println("Contratando Cuerpo Tecnico");
+                    System.out.println("Ingrese el nombre del cuerpo tecnico");
+                    String nombreTecnico=sc.next();
+                    System.out.println("Ingrese el salario base del cuerpo tecnico");
+                    double salarioBaseTecnico=sc.nextDouble();
+                    System.out.println("Ingrese el puesto del cuerpo tecnico");
+                    String puestoTecnico=sc.next();
+                    Tecnico tecnico=new Tecnico(nombreTecnico,salarioBaseTecnico,puestoTecnico);
+                    try{
+                        GestorClub.contratarTecnico(tecnico);
+                    }catch(PresupuestoExcedidoException e){
+                        System.err.println("Presupuesto excedido");
+                        System.err.println("Fichaje del cuerpo tecnico cancelado");
+                    }
                     break;
                 case 3:
                     System.out.println("Despedir profesional");
+                    System.out.println("Ingrese el nombre del profesional: ");
+                    String nombreProfesional=sc.next();
+                    try{
+                        GestorClub.despedirProfesional(nombreProfesional);
+                    }catch(ProfesionalNoEncontradoException e){
+                        System.err.println("Profesional no encontrado");
+                    }
                     break;
                 case 4:
-                    System.out.println("Ver nominas totales");
+                    System.out.println("Viendo nominas totales");
+
                     break;
                 case 5:
                     System.out.println("Programa finalizado");
                     bandera=false;
                     break;
+                case 6:
+                    GestorClub.mostrarProfesionales();
             }
         }
     }
